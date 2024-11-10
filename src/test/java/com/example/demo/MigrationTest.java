8d953f331e88a3ca174fc1706b8a91414e1b495e
joseTG <josetorresgarrido@gmail.com>
2024-11-10 03:04:37 -0500

    Code Tidy up

--- src/test/java/com/example/demo/MigrationTest.java
+++ src/test/java/com/example/demo/MigrationTest.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static com.example.demo.TestUtils.createTable;
+import static com.example.demo.TestUtils.insertItems;
+import static com.example.demo.TestUtils.scanByStatus;
+import static com.example.demo.TestUtils.scanAndUpdate;
 
 @SpringBootTest
 class MigrationTest extends AbstractBaseTest {
@@ -10,26 +14,26 @@ class MigrationTest extends AbstractBaseTest {
     @Test
     void demoTest() {
         int DEMO_RANGE = 15;
-        TestUtils.createTable(TABLE_NAME);
-        TestUtils.insertItems(TABLE_NAME, DEMO_RANGE);
-        TestUtils.scanByStatus(TABLE_NAME, DEMO_RANGE, V1_STATUS.STATUS_1);
-        TestUtils.scanByStatus(TABLE_NAME, DEMO_RANGE, V1_STATUS.STATUS_2);
-        TestUtils.scanByStatus(TABLE_NAME, DEMO_RANGE, V1_STATUS.STATUS_3);
-        TestUtils.scanByStatus(TABLE_NAME, DEMO_RANGE, V1_STATUS.STATUS_4);
+        createTable(TABLE_NAME);
+        insertItems(TABLE_NAME, DEMO_RANGE);
+        scanByStatus(TABLE_NAME, DEMO_RANGE, V1_STATUS.STATUS_1);
+        scanByStatus(TABLE_NAME, DEMO_RANGE, V1_STATUS.STATUS_2);
+        scanByStatus(TABLE_NAME, DEMO_RANGE, V1_STATUS.STATUS_3);
+        scanByStatus(TABLE_NAME, DEMO_RANGE, V1_STATUS.STATUS_4);
     }
 
     @Test
     void upsertAttributesTest() {
         int UPDATE_RANGE = 80;
-        TestUtils.createTable(TABLE_NAME); // comment this out when updating against environments
-        TestUtils.insertItems(TABLE_NAME, UPDATE_RANGE); // comment this out when updating against environments
-        TestUtils.scanByStatus(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_1);//
-        TestUtils.scanByStatus(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_2);//
-        TestUtils.scanByStatus(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_3);//
-        TestUtils.scanByStatus(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_4);//
-        TestUtils.scanAndUpdate(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_1, V2_STATUS.STATUS_A);
-        TestUtils.scanAndUpdate(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_2, V2_STATUS.STATUS_B);
-        TestUtils.scanAndUpdate(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_3, V2_STATUS.STATUS_C);
-        TestUtils.scanAndUpdate(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_4, V2_STATUS.STATUS_D);
+        createTable(TABLE_NAME); // comment this out when updating against environments
+        insertItems(TABLE_NAME, UPDATE_RANGE); // comment this out when updating against environments
+        scanByStatus(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_1);//
+        scanByStatus(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_2);//
+        scanByStatus(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_3);//
+        scanByStatus(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_4);//
+        scanAndUpdate(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_1, V2_STATUS.STATUS_A);
+        scanAndUpdate(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_2, V2_STATUS.STATUS_B);
+        scanAndUpdate(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_3, V2_STATUS.STATUS_C);
+        scanAndUpdate(TABLE_NAME, UPDATE_RANGE, V1_STATUS.STATUS_4, V2_STATUS.STATUS_D);
     }
 }
